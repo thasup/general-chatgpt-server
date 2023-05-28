a7a7797cb170d2f55d356d543d7ac475e8de542a
thasup <thanachonfirst@hotmail.com>
2023-05-29 00:48:13 +0700

    Add global utilize openai functions

---
+++ backend/utilities/openai.ts
@@ -0,0 +1,61 @@
+import dotenv from "dotenv";
+import { Configuration, OpenAIApi, type ChatCompletionRequestMessage } from "openai";
+
+import { colorsPaletteTextInstruction, colorsPaletteChatInstruction } from "../models/colors-palette.model";
+
+dotenv.config();
+const { OPENAI_API_KEY } = process.env;
+
+const configuration = new Configuration({
+  apiKey: OPENAI_API_KEY
+});
+const openai = new OpenAIApi(configuration);
+
+// GPT -3
+const createPromptForTextCompletion = (input: string, instruction?: (input: string) => string): string => {
+  if (instruction) {
+    return instruction(input);
+  }
+  return colorsPaletteTextInstruction(input);
+};
+
+const textCompletion = async (input: string, instruction?: (input: string) => string): Promise<string | undefined> => {
+  const completion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: createPromptForTextCompletion(input, instruction),
+    max_tokens: 500,
+    temperature: 0.7,
+    top_p: 0.5
+  });
+  const res = completion?.data?.choices[0]?.text;
+
+  return res;
+};
+
+// GPT - 3.5
+const createPromptForChatCompletion = (input: string, instruction?: (input: string) => ChatCompletionRequestMessage[]): ChatCompletionRequestMessage[] => {
+  if (instruction) {
+    return instruction(input);
+  }
+  return colorsPaletteChatInstruction(input);
+};
+
+const chatCompletion = async (input: string, instruction?: (input: string) => ChatCompletionRequestMessage[]): Promise<string | undefined> => {
+  const completion = await openai.createChatCompletion(
+    {
+      model: "gpt-3.5-turbo",
+      messages: createPromptForChatCompletion(input, instruction),
+      max_tokens: 500,
+      temperature: 0.7,
+      top_p: 0.5
+    }
+  );
+  const res = completion?.data?.choices[0]?.message?.content;
+
+  return res;
+};
+
+export {
+  textCompletion,
+  chatCompletion
+};

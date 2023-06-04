49ab05a49073e318f4071523ccdba7800a666d98
thasup <thanachonfirst@hotmail.com>
2023-06-05 05:05:03 +0700

    Comment static files route

--- backend/server.ts
+++ backend/server.ts
@@ -33,26 +33,27 @@ app.use((req: Request, res: Response, next) => {
   console.log(`${req.method} ${req.baseUrl}${req.url} ${delta}ms`);
 });
 
-app.use(express.static(path.join(
-  __dirname.replace("/dist", ""),
-  "/frontend",
-  "/colors",
-  "/public"
-)));
+// app.use(express.static(path.join(
+//   __dirname.replace("/dist", ""),
+//   "/frontend",
+//   "/colors",
+//   "/public"
+// )));
 
 app.get("/", (req: Request, res: Response) => {
   res.send("API is running...");
 });
 
-app.get("/colors", (req: Request, res: Response) => {
-  res.sendFile(path.resolve(
-    __dirname.replace("/dist", ""),
-    "frontend",
-    "colors",
-    "public",
-    "index.html"
-  ));
-});
+// app.get("/colors", (req: Request, res: Response) => {
+//   res.sendFile(path.resolve(
+//     __dirname.replace("/dist", ""),
+//     "frontend",
+//     "colors",
+//     "public",
+//     "index.html"
+//   ));
+// });
+
 app.use("/v1/colors", colorsPaletteRouter);
 app.use("/v1/manga", mangaComparatorRouter);
 app.use("/v1/spotify", spotifyPlaylistRouter);
